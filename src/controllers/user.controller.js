98e6e1b2e94158114e46dc87a505af942a83f2f9
Anurag Yadav <yadavanurag1310@gmail.com>
2024-04-25 19:57:30 +0530

    getCurrentUser funcitonality setup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -300,9 +300,22 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
     .json(new ApiResponse(200, {}, "Password changed successfully"))
 })
 
+const getCurrentUser = asyncHandler(async(req, res) => {
+
+    return res
+    .status(200)
+    .json(
+        200,
+        req.user,
+        "Current User fectched successfully"
+    )
+})
+
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken, 
+    changeCurrentPassword,
+    getCurrentUser
 }
