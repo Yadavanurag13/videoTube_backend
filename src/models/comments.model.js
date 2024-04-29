103ae0f22069adede8c5dff1bc5afd431b47ecd6
Anurag Yadav <yadavanurag1310@gmail.com>
2024-04-30 01:16:57 +0530

    added comment model

---
+++ src/models/comments.model.js
@@ -0,0 +1,25 @@
+import mongoose, {Schema} from "mongoose"
+
+const commentSchema = new Schema(
+    {
+        content: {
+            type: String,
+            required: true
+        },
+        video: {
+            type: Schema.Types.ObjectId,
+            ref: "Video",
+            required: true
+        },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const comment = new mongoose.model("Comment", commentSchema)
